282e7ce5ee90c6f45c260351c0cc1fe5477824e1
Kasper <kbielecki11601@gmail.com>
2020-12-01 23:51:49 -0500

    Final Submission

--- content.js
+++ content.js
@@ -1,15 +1,85 @@
-chrome.storage.sync.get({fromStored:"", toStored:""}, function(result) {
-
   var html = document.querySelector('html');
   var walker = document.createTreeWalker(html,NodeFilter.SHOW_TEXT);
 
   var node;
   while (node = walker.nextNode()) {
-    var re = new RegExp(result.fromStored,"gi");
-    node.nodeValue = node.nodeValue.replace(re,result.toStored);
+//1
+    node.nodeValue = node.nodeValue.replace(/Nvidia Shield TV/gi, `Nvidia Shield TV *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Shield TV/gi, `Shield TV *privacy not included`)
+    node.nodeValue = node.nodeValue.replace(/Shield TV Pro/gi, `Shield TV Pro*privacy not included`)
+//2
+    node.nodeValue = node.nodeValue.replace(/Livescribe Smartpen/gi, `Livescribe Smartpen *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Livescribe Symphony/gi, `Livescribe Symphony *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Livescribe Echo/gi, `Livescribe Echo *privacy not included`);
+//3
+    node.nodeValue = node.nodeValue.replace(/Dyson Pure Cool/gi, `Dyson Pure Cool *privacy not included`);
+//4
+    node.nodeValue = node.nodeValue.replace(/Kobo eReaders/gi, `Kobo eReaders *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Kobo Forma/gi, `Kobo Forma *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Kobo Libra H2O/gi, `Kobo Libra H2O *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Kobo Clara HD/gi, `Kobo Clara HD *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Kobo Nia/gi, `Kobo Nia *privacy not included`);
+    node.nodeValue = node.nodeValue.replace(/Kobo Aura/gi, `Kobo Aura *privacy not included`);
+//5
+    node.nodeValue = node.nodeValue.replace(/Roku/gi, `Roku *privacy not included`);
+//6
+    node.nodeValue = node.nodeValue.replace(/Tonal/gi, `Tonal *privacy not included`);
+//7
+    node.nodeValue = node.nodeValue.replace(/Artie 3000 Coding Robot/gi, `Artie 3000 Coding Robot *privacy not included`);
+//8
+    node.nodeValue = node.nodeValue.replace(/Schlage Sense Smart Deadbolt/gi, `Schlage Sense Smart Deadbolt *privacy not included`);
+//9
+    node.nodeValue = node.nodeValue.replace(/Hamilton Beach Smart Coffee Maker/gi, `Hamilton Beach Smart Coffee Maker *privacy not included`);
+//9
+    node.nodeValue = node.nodeValue.replace(/Levoit Core 200s/gi, `Levoit Smart True Purifiers`);
+//10
+    node.nodeValue = node.nodeValue.replace(/Greater Goods WiFi Smart Body Scale/gi, `Greater Goods WiFi Smart Body Scale *privacy not included`);
+//11
+    node.nodeValue = node.nodeValue.replace(/Wickedbone Interactive Gaming Toy For Dogs/gi, `Wickedbone Interactive Gaming Toy For Dogs *privacy not included`);
+//12
+    node.nodeValue = node.nodeValue.replace(/Huawei Honor Band 5/gi, `Huawei Honor Band 5 *privacy not included`);
+//13
+    node.nodeValue = node.nodeValue.replace(/Huawei Smart Watch ES/gi, `Huawei Smart Watch ES *privacy not included`);
+//14
+    node.nodeValue = node.nodeValue.replace(/Ring Video Doorbell/gi, `Ring Video Doorbell *privacy not included`);
+//15
+    node.nodeValue = node.nodeValue.replace(/DJI Mavic Mini/gi, `DJI Mavic Mini *privacy not included`);
+//16
+    node.nodeValue = node.nodeValue.replace(/Mi Band 5/gi, `Mi Band 5 *privacy not included`);
+//17
+    node.nodeValue = node.nodeValue.replace(/Coway Airmega 300S/gi, `Coway Airmega 300S *privacy not included`);
+//18
+    node.nodeValue = node.nodeValue.replace(/Coway Airmega 400S/gi, `Coway Airmega 400S *privacy not included`);
+//19
+    node.nodeValue = node.nodeValue.replace(/Simplisafe Security Camera/gi, `Simplisafe Security Camera *privacy not included`);
+//20
+    node.nodeValue = node.nodeValue.replace(/Schlage Encode Smart WiFi Deadbolt/gi, `Schlage Encode Smart WiFi Deadbolt *privacy not included`);
+//21
+    node.nodeValue = node.nodeValue.replace(/SpotOn Fence/gi, `SpotOn Fence *privacy not included`);
+//22
+    node.nodeValue = node.nodeValue.replace(/Ring Security Camera/gi, `Ring Security Camera *privacy not included`);
+//23
+    node.nodeValue = node.nodeValue.replace(/Amazfit Band 5/gi, `Amazfit Band 5 *privacy not included`);
+//25
+    node.nodeValue = node.nodeValue.replace(/Ubtech/gi, `Ubtech *privacy not included`);
+//26
+    node.nodeValue = node.nodeValue.replace(/Atomi Smart Coffee Maker/gi, `Atomi Smart Coffee Maker *privacy not included`);
+//27
+    node.nodeValue = node.nodeValue.replace(/Facebook Portal/gi, `Facebook Portal *privacy not included`);
+//28
+    node.nodeValue = node.nodeValue.replace(/Moleskine Smart Writing Set/gi, `Moleskine Smart Writing Set *privacy not included`);
+//29
+    node.nodeValue = node.nodeValue.replace(/ikuddle Auto-Pack Litter Box/gi, `ikuddle Auto-Pack Litter Box *privacy not included`);
+//30
+    node.nodeValue = node.nodeValue.replace(/KidKraft Amazon Alexa 2-in-1 Kitchen & Market/gi, `KidKraft Amazon Alexa 2-in-1 Kitchen & Market *privacy not included`);
+//31
+    node.nodeValue = node.nodeValue.replace(/Oculus Quest 2 VR Headset/gi, `Oculus Quest 2 VR Headset *privacy not included`);
+//32
+    node.nodeValue = node.nodeValue.replace(/Dogness iPet Robot/gi, `Dogness iPet Robot *privacy not included`);
+//33
+    node.nodeValue = node.nodeValue.replace(/Amazon Halo/gi, `Amazon Halo *privacy not included`);
+
   }
-  
-});
 
 
-//node.nodeValue = node.nodeValue.replace(/ now /gi, ` Now this is a story all about how My life got flipped-turned upside down And I'd like to take a minute Just sit right there I'll tell you how I became the prince of a town called Bel-Air In west Philadelphia born and raised On the playground was where I spent most of my days Chillin' out maxin' relaxin' all cool And all shooting some b-ball outside of the school When a couple of guys who were up to no good Started making trouble in my neighborhood I got in one little fight and my mom got scared She said, "You're movin' with your auntie and uncle in Bel-Air" I begged and pleaded with her day after day But she packed my suitcase and sent me on my way She gave me a kiss and then she gave me my ticket I put my Walkman on and said "I might as well kick it" First class, yo, this is bad Drinking orange juice out of a champagne glass Is this what the people of Bel-Air living like? Hmm, this might be alright But wait I hear they're prissy, bourgeois, all that Is this the type of place that they just send this cool cat? I don't think so I'll see when I get there I hope they're prepared for the prince of Bel-Air Well, the plane landed and when I came out There was a dude who looked like a cop Standing there with my name out I ain't trying to get arrested yet I just got here I sprang with the quickness like lightning, disappeared I whistled for a cab and when it came near The license plate said "Fresh" and it had dice in the mirror If anything I could say that this cab was rare But I thought, "Nah, forget it" – "Yo, home to Bel-Air" I pulled up to the house about 7 or 8 And I yelled to the cabbie "Yo home smell ya later" I looked at my kingdom I was finally there To sit on my throne as the Prince of Bel-Air `)
+//
